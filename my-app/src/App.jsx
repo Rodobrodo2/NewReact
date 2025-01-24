bc7dce6f7bfd50507f9c69bcc65734ee851a1f2f
Rodobrodo2 <rodondolo2@gmail.com>
2025-01-24 11:38:48 +0100

    Aggiunti collegamenti tra pagine

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,16 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import React from "react";
-import About from "./components/About";
-import Home from "./components/Home";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Nav from "./components/Nav";
 
 const App = () => {
   return (
-    <>
+    <div>
+      <Nav />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<div></div>} /> {/* Rotta vuota */}
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
---
+++ my-app/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/">Main Page</Link></li>
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
